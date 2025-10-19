26e8731842a75188dd59f6809a2a0f1ddae4eab5
irrynka <irinakorcinska7@gmail.com>
2025-10-19 19:38:49 +0200

    CREATE TABLES

---
+++ main.py
@@ -0,0 +1,31 @@
+import sqlite3
+conn = sqlite3.connect("university.db")
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER,
+    major TEXT
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS courses (
+    course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    course_name TEXT,
+    instructor TEXT
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS student_courses (
+    student_id,
+    course_id INTEGER,
+    FOREIGN KEY(student_id) REFERENCES students(id),
+    FOREIGN KEY(course_id) REFERENCES courses(id),
+    PRIMARY KEY (student_id,course_id)
+)
+''')
+
